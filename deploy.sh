8f02b2837f69b17411e30c66b889f6c558bee19e
Daniele Gazzelloni <daniele@danielegazzelloni.com>
2019-07-16 11:46:12 +0100

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 UPDATE_AT=+%s
-
-rm .DS_Store
+FULL_DATE=`date -u +%FT%T.000Z`
+rm .DS_Store 2> /dev/null
 
 date -u $UPDATE_AT > update_at.date
-
+echo "Update: ${FULL_DATE}"
 git add .
 git commit -m "Update: $UPDATE_AT"
 git push
