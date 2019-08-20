bcda1d12130ee633345dedc06e2a5ec082c6b15d
Daniele Gazzelloni <daniele@danielegazzelloni.com>
2019-08-20 13:36:15 +0100

    Mobile navigation

--- v2/src/components/sidebar.vue
+++ v2/src/components/sidebar.vue
@@ -1,71 +1,89 @@
 <template>
   <aside class="sidebar">
-    <!-- logo -->
-    <div class="logo">
-      <nuxt-link to="/">D</nuxt-link>
-    </div>
+    <nav>
+      <!-- logo -->
+      <div class="logo">
+        <nuxt-link to="/" @click.native="isBurgerExpanded = false">D</nuxt-link>
+      </div>
 
-    <!-- menu -->
-    <div class="menu">
-      <!-- home -->
-      <span class="icon is-medium">
-        <nuxt-link to="/">
-          <i class="la la-home"></i>
-        </nuxt-link>
-        <span class="hover-text">home</span>
-      </span>
+      <!-- menu -->
+      <div :class="{ menu: true, 'is-expanded': isBurgerExpanded }">
+        <!-- home -->
+        <span class="icon is-medium">
+          <nuxt-link to="/" @click.native="isBurgerExpanded = false">
+            <i class="la la-home"></i>
+          </nuxt-link>
+          <span class="hover-text">home</span>
+        </span>
 
-      <!-- about -->
-      <span class="icon is-medium">
-        <nuxt-link to="/about">
-          <i class="la la-user"></i>
-        </nuxt-link>
-        <span class="hover-text">about</span>
-      </span>
+        <!-- about -->
+        <span class="icon is-medium">
+          <nuxt-link to="/about" @click.native="isBurgerExpanded = false">
+            <i class="la la-user"></i>
+          </nuxt-link>
+          <span class="hover-text">about</span>
+        </span>
 
-      <!-- skills -->
-      <span class="icon is-medium">
-        <nuxt-link to="/skills">
-          <i class="la la-gears"></i>
-        </nuxt-link>
-        <span class="hover-text">skills</span>
-      </span>
+        <!-- skills -->
+        <span class="icon is-medium">
+          <nuxt-link to="/skills" @click.native="isBurgerExpanded = false">
+            <i class="la la-gears"></i>
+          </nuxt-link>
+          <span class="hover-text">skills</span>
+        </span>
 
-      <!-- my work -->
-      <span class="icon is-medium">
-        <nuxt-link to="/my-work">
-          <i class="la la-eye"></i>
-        </nuxt-link>
-        <span class="hover-text">my work</span>
-      </span>
+        <!-- my work -->
+        <span class="icon is-medium">
+          <nuxt-link to="/my-work" @click.native="isBurgerExpanded = false">
+            <i class="la la-eye"></i>
+          </nuxt-link>
+          <span class="hover-text">my work</span>
+        </span>
 
-      <!-- contact -->
-      <span class="icon is-medium">
-        <nuxt-link to="/contact">
-          <i class="la la-envelope"></i>
-        </nuxt-link>
-        <span class="hover-text">contact</span>
-      </span>
-    </div>
+        <!-- contact -->
+        <span class="icon is-medium">
+          <nuxt-link to="/contact" @click.native="isBurgerExpanded = false">
+            <i class="la la-envelope"></i>
+          </nuxt-link>
+          <span class="hover-text">contact</span>
+        </span>
+      </div>
 
-    <!-- socials -->
-    <div class="socials">
-      <span class="icon">
-        <a href="https://github.com/dangzo" target="_blank">
-          <i class="fa fa-github"></i>
-        </a>
-      </span>
-      <span class="icon">
-        <a href="https://www.linkedin.com/in/danielegazzelloni" target="_blank">
-          <i class="fa fa-linkedin"></i>
-        </a>
-      </span>
-      <span class="icon">
-        <nuxt-link to="/contact">
-          <i class="la la-envelope"></i>
-        </nuxt-link>
-      </span>
-    </div>
+      <!-- socials -->
+      <div class="socials">
+        <span class="icon">
+          <a href="https://github.com/dangzo" target="_blank">
+            <i class="fa fa-github"></i>
+          </a>
+        </span>
+        <span class="icon">
+          <a href="https://www.linkedin.com/in/danielegazzelloni" target="_blank">
+            <i class="fa fa-linkedin"></i>
+          </a>
+        </span>
+        <span class="icon">
+          <nuxt-link to="/contact" @click.native="isBurgerExpanded = false">
+            <i class="la la-envelope"></i>
+          </nuxt-link>
+        </span>
+      </div>
+
+      <!-- hamburger -->
+      <a
+        :class="{
+          'navbar-burger': true,
+          'is-active': isBurgerExpanded,
+        }"
+        :aria-expanded="isBurgerExpanded"
+        role="button"
+        aria-label="menu"
+        @click="doBurgerClick"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </nav>
   </aside>
 </template>
 
@@ -73,7 +91,14 @@
 import { Component, Vue } from 'nuxt-property-decorator';
 
 @Component
-export default class Sidebar extends Vue {}
+export default class Sidebar extends Vue {
+  // Local state
+  isBurgerExpanded = false;
+
+  doBurgerClick(): void {
+    this.isBurgerExpanded = !this.isBurgerExpanded;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -83,10 +108,33 @@ export default class Sidebar extends Vue {}
   width: 60px;
   position: fixed;
   text-align: center;
+  z-index: 2;
 
   @media (max-width: 1100px) {
     width: 100%;
-    height: 60px;
+    height: 58px;
+  }
+
+  @media (max-width: 768px) {
+    height: 51px;
+  }
+
+  .navbar-burger {
+    top: 0.2rem;
+    margin-right: 1.5rem;
+
+    @media (min-width: 520px) {
+      display: none;
+    }
+
+    @media (max-width: 450px) {
+      margin-right: 1.2rem;
+    }
+
+    span {
+      color: $grey;
+      height: 2px;
+    }
   }
 
   .icon {
@@ -128,6 +176,10 @@ export default class Sidebar extends Vue {}
       transform: translateY(-50%);
     }
 
+    @media (max-width: 450px) {
+      margin: 0 2.4rem;
+    }
+
     a:hover {
       color: $primary;
     }
@@ -144,9 +196,33 @@ export default class Sidebar extends Vue {}
       transform: translateX(-50%);
     }
 
+    @media (max-width: 520px) {
+      display: flex;
+      position: fixed;
+      opacity: 0;
+      height: 39px;
+      width: 100%;
+      top: 50px;
+      padding-left: 0.8rem;
+      padding-right: 0.8rem;
+      background-color: $deep-dark;
+      -webkit-transition: opacity 400ms ease-out;
+      -moz-transition: opacity 400ms ease-out;
+      -o-transition: opacity 400ms ease-out;
+      transition: opacity 400ms ease-out;
+
+      &.is-expanded {
+        opacity: 1;
+      }
+    }
+
     .icon {
       position: relative;
       margin: 0.8rem auto;
+
+      @media (max-width: 520px) {
+        margin: 0.4rem auto;
+      }
 
       a {
         background-color: $deep-dark;
@@ -204,6 +280,10 @@ export default class Sidebar extends Vue {}
       transform: translateY(-50%);
       bottom: 0;
       right: 2rem;
+    }
+
+    @media (max-width: 520px) {
+      display: none;
     }
 
     .icon {
